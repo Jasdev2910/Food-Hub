e1d64c1366a37e6babd157bea0a1d061cf28fbb8
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-09-29 18:47:56 +0530

    help section updated

--- src/components/Help.js
+++ src/components/Help.js
@@ -1,11 +1,16 @@
+import Accordian, { FaqAccordian } from "./Accordian";
+import FaqData from "../utils/FaqData";
+
 const Help = () => {
+  const HelpAccordian = FaqAccordian();
   return (
     <div className="felx-col">
       <h1>Help & Support</h1>
       <h2>Let's take a step ahead & help you better</h2>
-      <div className="flex">
-        <div className="w-96 h-96 bg-slate-300">hello</div>
-        <div className="w-64 h-96 bg-slate-200">world</div>
+      <div>
+        {FaqData.map((obj) => (
+          <HelpAccordian data={obj} />
+        ))}
       </div>
     </div>
   );
