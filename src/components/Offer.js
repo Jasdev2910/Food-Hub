624662cb888612e27b83cdf257f67337d0bb3551
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-10-29 20:52:32 +0530

    Shimmer removed

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,7 +1,6 @@
 import OfferCard from "./OfferCard";
 import React from "react";
 import { useEffect } from "react";
-import ShimmerMenu from "./ShimmerMenu";
 
 const Offer = () => {
   useEffect(() => {
@@ -12,7 +11,6 @@ const Offer = () => {
       <h1>Offers Available</h1>
       <OfferCard offer={"Get 100 Cashback"} condition={"order above 400"} />
       <OfferCard offer={"Get 50% Discount"} condition={"order above 300"} />
-      <ShimmerMenu />
     </div>
   );
 };
