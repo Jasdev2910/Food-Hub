207476339e9bb9e3edc392815151f4bf9ae7bb9f
Jasdev2910 <jasdevvirdi987@gmail.com>
2023-11-04 20:39:34 +0530

    offer component updated

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,16 +1,48 @@
 import OfferCard from "./OfferCard";
 import React from "react";
 import { useEffect } from "react";
+import Card, { withOfferLabel } from "./Card";
+import { Link } from "react-router-dom";
+
+import useRestaurantList from "../utils/useRestaurantList";
+import Shimmer from "./Shimmer";
 
 const Offer = () => {
+  const CardWithOffer = withOfferLabel(Card);
+  const restaurantData = useRestaurantList(); // custom hook for list of restauranrt
+  const restaurantList =
+    restaurantData[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return (
-    <div>
-      <h1>Offers Available</h1>
-      <OfferCard offer={"Get 100 Cashback"} condition={"order above 400"} />
-      <OfferCard offer={"Get 50% Discount"} condition={"order above 300"} />
+  return restaurantList?.length === undefined ? (
+    <Shimmer />
+  ) : (
+    <div className="flex-col mx-24 my-10">
+      <div className="text-2xl font-bold">
+        <h1>Restaurant With Great Offer Near Me</h1>
+      </div>
+      <div className="flex flex-wrap flex-row justify-between px-4 py-5 ">
+        {restaurantList?.map((restaurant) => (
+          <Link
+            key={restaurant?.info?.id}
+            // onClick={() => handleClick(restaurant)}
+            to={"/restaurant/" + restaurant?.info?.id}
+          >
+            <CardWithOffer
+              imageId={restaurant?.info?.cloudinaryImageId}
+              name={restaurant?.info?.name}
+              avgRating={restaurant?.info?.avgRating}
+              cuisines={restaurant?.info?.cuisines}
+              areaName={restaurant?.info?.areaName}
+              labelHeader={restaurant?.info?.aggregatedDiscountInfoV3?.header}
+              labelSubHeader={
+                restaurant?.info?.aggregatedDiscountInfoV3?.subHeader
+              }
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
